790632ba51ac2a86466ece1a3d74c5f38338536e
Aditya Raj <aditya.x510@gmail.com>
2019-01-09 11:51:25 +0530

    Initial commit

---
+++ basic/dataTypes.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello world \n";
+# variables in php
+# variable decleration
+$var_int = 10;   # integer datatype
+$var_str = "Aditya"; # string datatype
+$var_float = 10.12;  # float datatype
+$var_undefined;  # undefined datatype
+$var_boolean = true;
+
+
+echo $var_int, "\n";
+echo $var_str, "\n";
+echo $var_float, "\n";
+echo $var_undefined, "\n";
+echo $var_boolean, "\n";
+
+?>
+

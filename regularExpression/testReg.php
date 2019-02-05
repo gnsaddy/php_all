46a75052ffd0639f7f6ca4ecab7eaac55f17aebc
Aditya Raj <aditya.x510@gmail.com>
2019-02-05 15:53:35 +0530

    Update testReg.php

--- regularExpression/testReg.php
+++ regularExpression/testReg.php
@@ -5,11 +5,10 @@
 </head>
 <body>
     
-
-
 <?php
 $pattern = "/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/";
 $text = $_POST['ph'];
+echo "Mobile Number is : ". $_POST['ph'];
 if(preg_match($pattern, $text)){
     echo "Match found!";
 } else{

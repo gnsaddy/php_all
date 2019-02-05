fc8548cf198e50692ecd6e06ffef419664d8ab0b
Aditya Raj <aditya.x510@gmail.com>
2019-02-05 15:52:24 +0530

    initial

---
+++ regularExpression/testReg.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <pre>Match a Pattern against a Phone Number Using RegExp in PHP</pre>
+</head>
+<body>
+    
+
+
+<?php
+$pattern = "/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/";
+$text = $_POST['ph'];
+if(preg_match($pattern, $text)){
+    echo "Match found!";
+} else{
+    echo "Match not found.";
+}
+?>
+
+</body>
+</html>  
